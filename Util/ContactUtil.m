aebbccd7725fa98c99202f3dd8eecc98f2dd8593
xialeistudio <xialeistudio@gmail.com>
2015-12-24 15:39:50 +0800

    保存联系人OK

--- Util/ContactUtil.m
+++ Util/ContactUtil.m
@@ -17,17 +17,31 @@
         NSArray *contacts = [store unifiedContactsMatchingPredicate:[CNContact predicateForContactsMatchingName:name] keysToFetch:@[[CNContactViewController descriptorForRequiredKeys]] error:nil];
         return contacts.count > 0;
     } else {
-        ABAddressBookRef addressBook = ABAddressBookCreate();
-//等待同意后向下执行
+        ABAddressBookRef addressBook = nil;
+        addressBook = ABAddressBookCreateWithOptions(NULL, NULL);
+        //等待同意后向下执行
         dispatch_semaphore_t sema = dispatch_semaphore_create(0);
         ABAddressBookRequestAccessWithCompletion(addressBook, ^(bool granted, CFErrorRef error) {
             dispatch_semaphore_signal(sema);
         });
         dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
-        CFArrayRef searchedPerson = ABAddressBookCopyPeopleWithName(addressBook, (__bridge CFStringRef) name);
-        NSArray *array = (__bridge NSArray *) searchedPerson;
-        CFRelease(searchedPerson);
-        return array.count > 0;
+        CFArrayRef records;
+        if (addressBook) {
+            records = ABAddressBookCopyArrayOfAllPeople(addressBook);
+        } else {
+#ifdef DEBUG
+            NSLog(@"can not connect to address book");
+#endif
+            return YES;
+        }
+        for (int i = 0; i < CFArrayGetCount(records); i++) {
+            ABRecordRef record = CFArrayGetValueAtIndex(records, i);
+            CFStringRef cfString = ABRecordCopyValue(record, kABPersonFirstNameProperty);
+            if ([(__bridge NSString *) cfString isEqualToString:name]) {
+                return YES;
+            }
+        }
+        return NO;
     }
 }
 
@@ -53,25 +67,33 @@
         [viewController presentViewController:navigationController animated:YES completion:nil];
         return NO;
     } else {
-        ABAddressBookRef addressBook = ABAddressBookCreate();
-        ABRecordRef record = ABPersonCreate();
-        ABRecordSetValue(record, kABPersonFirstNameProperty, (__bridge CFTypeRef) realname, NULL);
-        ABRecordSetValue(record, kABPersonJobTitleProperty, (__bridge CFTypeRef) position, NULL);
-        ABRecordSetValue(record, kABPersonOrganizationProperty, (__bridge CFTypeRef) company, NULL);
-        //手机号码
-        ABMultiValueRef phoneNumbers = ABMultiValueCreateMutable(kABMultiStringPropertyType);
-        ABMultiValueAddValueAndLabel(phoneNumbers, (__bridge CFTypeRef) phone, kABPersonPhoneMainLabel, NULL);
-        ABRecordSetValue(record, kABPersonPhoneProperty, phoneNumbers, NULL);
-        //头像
+        ABAddressBookRef addressBook = ABAddressBookCreateWithOptions(NULL, NULL);
+        dispatch_semaphore_t sema = dispatch_semaphore_create(0);
+        ABAddressBookRequestAccessWithCompletion(addressBook, ^(bool granted, CFErrorRef error) {
+            dispatch_semaphore_signal(sema);
+        });
+        dispatch_semaphore_wait(sema, DISPATCH_TIME_FOREVER);
+
+        // 创建一条空的联系人
+        ABRecordRef recordRef = ABPersonCreate();
+        ABRecordSetValue(recordRef, kABPersonFirstNameProperty, (__bridge CFTypeRef) realname, NULL);
+        ABRecordSetValue(recordRef, kABPersonJobTitleProperty, (__bridge CFTypeRef) position, NULL);
+        ABRecordSetValue(recordRef, kABPersonOrganizationProperty, (__bridge CFTypeRef) company, NULL);
         NSData *imageData = [NSData dataWithContentsOfURL:[NSURL URLWithString:avatar]];
-        ABPersonSetImageData(record, (__bridge CFDataRef) imageData, NULL);
+        ABPersonSetImageData(recordRef, (__bridge CFDataRef) imageData, NULL);
 
-        BOOL result = ABAddressBookAddRecord(addressBook, record, NULL);
-        //释放对象
-        CFRelease(record);
-        CFRelease(addressBook);
-        CFRelease(phoneNumbers);
+        ABMutableMultiValueRef multiValueRef = ABMultiValueCreateMutable(kABStringPropertyType);//添加设置多值属性
+        ABMultiValueAddValueAndLabel(multiValueRef, (__bridge CFStringRef) (phone), kABWorkLabel, NULL);//添加工作电话
+        ABRecordSetValue(recordRef, kABPersonPhoneProperty, multiValueRef, NULL);
 
+        //添加记录
+        ABAddressBookAddRecord(addressBook, recordRef, NULL);
+
+        //保存通讯录，提交更改
+        BOOL result = ABAddressBookSave(addressBook, NULL);
+        //释放资源
+        CFRelease(recordRef);
+        CFRelease(multiValueRef);
         return result;
     }
 }
